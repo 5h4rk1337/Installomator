a334513f3531d53fe772a07a9001f98bc65446e0
5h4rk1337 <31519324+5h4rk1337@users.noreply.github.com>
2025-09-10 11:13:43 +0200

    Update duet.sh
    Name is now in lowercase because it is also lowercase in the downloaded .dmg.
    Type has changed from zip to dmg.
    Fixed appNewVersion.

--- fragments/labels/duet.sh
+++ fragments/labels/duet.sh
@@ -1,8 +1,8 @@
 duet)
-    name="Duet"
-    type="zip"
+    name="duet"
+    type="dmg"
     downloadURL="https://updates.duetdisplay.com/AppleSilicon"
-    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f6 | sed 's/duet-//' | sed 's/.zip//' | sed 's/-/./g')"
+    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i '^location:' | sed -E 's/.*duet-dd-([0-9-]+)\.dmg.*/\1/' | tr '-' '.')"
     expectedTeamID="J6L96W8A86"
     blockingProcesses=( "duet" "duet Networking" )
     ;;
